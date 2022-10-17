aa782a6109e0f2ce2416c12a7dc672f8c3600226
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-17 16:35:04 +0200

    3 replikas

--- src/main/kotlin/no/nav/helse/flex/inntektskilder/SykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/helse/flex/inntektskilder/SykepengesoknadListener.kt
@@ -4,18 +4,14 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.objectMapper
 import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
-import java.time.LocalDate
-import java.time.ZoneOffset
 
 @Component
 class SykepengesoknadListener(
     private val nyttGenerertSporsmal: NyttGenerertSporsmal,
-) : ConsumerSeekAware {
+) {
 
     @KafkaListener(
         topics = [FLEX_SYKEPENGESOKNAD_TOPIC],
@@ -30,14 +26,6 @@ class SykepengesoknadListener(
         nyttGenerertSporsmal.finnNyttSporsmal(soknad)
 
         acknowledgment.acknowledge()
-    }
-
-    override fun onPartitionsAssigned(
-        assignments: MutableMap<TopicPartition, Long>,
-        callback: ConsumerSeekAware.ConsumerSeekCallback
-    ) {
-        val startDateInEpochMilli = LocalDate.of(2022, 10, 10).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
-        callback.seekToTimestamp(assignments.map { it.key }, startDateInEpochMilli)
     }
 }
 
