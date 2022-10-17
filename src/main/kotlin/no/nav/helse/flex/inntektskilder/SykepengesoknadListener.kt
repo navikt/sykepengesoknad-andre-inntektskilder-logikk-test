4e4e11af218ea670e0b13ffe9c204961e10bdc8f
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-17 16:26:34 +0200

    Rekjør uten frilansere

--- src/main/kotlin/no/nav/helse/flex/inntektskilder/SykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/helse/flex/inntektskilder/SykepengesoknadListener.kt
@@ -4,22 +4,24 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.objectMapper
 import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.context.annotation.Profile
+import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 @Component
-@Profile("test")
 class SykepengesoknadListener(
     private val nyttGenerertSporsmal: NyttGenerertSporsmal,
-) {
+) : ConsumerSeekAware {
 
     @KafkaListener(
         topics = [FLEX_SYKEPENGESOKNAD_TOPIC],
         id = "nytt-generert-sporsmal-listener",
         idIsGroup = false,
-        groupId = "nytt-generert-inntektskilde-sporsmal-2"
+        groupId = "nytt-generert-inntektskilde-sporsmal-3"
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
 
@@ -28,6 +30,14 @@ class SykepengesoknadListener(
         nyttGenerertSporsmal.finnNyttSporsmal(soknad)
 
         acknowledgment.acknowledge()
+    }
+
+    override fun onPartitionsAssigned(
+        assignments: MutableMap<TopicPartition, Long>,
+        callback: ConsumerSeekAware.ConsumerSeekCallback
+    ) {
+        val startDateInEpochMilli = LocalDate.of(2022, 10, 10).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
+        callback.seekToTimestamp(assignments.map { it.key }, startDateInEpochMilli)
     }
 }
 
